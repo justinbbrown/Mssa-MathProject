883ee0c7e5cf2cf401b99ab6b3c0de2c710ab50f
Justin Brown <32085382+justinbbrown@users.noreply.github.com>
2019-04-28 11:39:59 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿
+using System;
+
+
+namespace ExceptionHandling
+{
+    public class NoNegativeZeroException : Exception
+    {
+        public NoNegativeZeroException() : base(String.Format("NoNegativeZeroException Generated: The number entered is less than/= 0")) { }
+    }
+
+    public class FindingCircumferenceAndArea
+    {
+        double userRadiusInput = Convert.ToDouble(Console.ReadLine());
+        public double MathPi = 3.14159;
+        public int PreDeterminedCircleDiameter = 2;
+
+        public void GenerateExceptions()
+        {
+
+            if (userRadiusInput <= 0)
+            {
+                throw new NoNegativeZeroException();
+            }
+            
+        }
+
+        public double CalculateCircleArea()
+        {
+            GenerateExceptions();           
+            return  MathPi * (userRadiusInput * userRadiusInput);       
+            
+        }
+
+        public double CalculateCircleCircumference()
+        {
+            GenerateExceptions();            
+            return PreDeterminedCircleDiameter * MathPi * userRadiusInput;                    
+        }
+
+    }
+         
+
+    class Program
+    {
+        
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, this application calculates the Area & Circumference of a circle. Please enter radius of circle:");
+            Console.WriteLine("********************************************************************");
+
+            try
+            {
+                FindingCircumferenceAndArea AreaCircumference = new FindingCircumferenceAndArea();
+
+                Console.WriteLine("Area:{0}", AreaCircumference.CalculateCircleArea());
+
+                Console.WriteLine("Circumference:{0}", AreaCircumference.CalculateCircleCircumference());
+            }
+            catch (NoNegativeZeroException)
+            {                
+                throw; 
+            }
+
+        }
+    }
+}
+
